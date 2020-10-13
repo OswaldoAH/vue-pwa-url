ed9585079dfb022791fb9c7f41e62fe5f0f3c3c1
Oswaldo Alvarez <mynor4755@hotmail.com>
2020-10-13 15:40:46 -0600

    ADD: NavBar Se configuró bootstrap para trabajar con vue Se añadió el navbar

---
+++ src/components/Navigation.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav>
+    <b-navbar toggleable="md" type="dark" class="color">
+      <b-container>
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-navbar-brand href="#">
+          <img src="../assets/logo.png" alt="Guate Viaja" />
+        </b-navbar-brand>
+        <b-collapse is-nav id="nav_collapse">
+          <b-navbar-nav>
+            <b-nav-item href="#">Inicio</b-nav-item>
+            <b-nav-item href="#visit">Lo más visitado</b-nav-item>
+            <b-nav-item href="#featured">Destacado</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+nav {
+  position: fixed;
+  width: 100%;
+}
+.color {
+  background-color: rgb(6, 73, 102);
+}
+img{
+    height: 50px;
+}
+</style>
