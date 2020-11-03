05e208e260cb7ab6a7c2ebf7753481e85ff66ab8
Oswaldo Alvarez <mynor4755@hotmail.com>
2020-11-03 11:29:13 -0600

    ADD:Custom Card Se creó el componente card para pasarle los datos

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,11 @@
       <navigation />
     </header>
     <main>
-      
+      <div class="container">
+        <div class="row">
+          <customCard v-for="(place, index) in places" :key="index" :place=place />
+        </div>
+      </div>
     </main>
     <footer>
       <customFooter />
@@ -15,11 +19,60 @@
 <script>
 import Navigation from "./components/Navigation";
 import CustomFooter from "./components/CustomFooter";
+import CustomCard from "./components/CustomCard";
+
 export default {
   name: "app",
   components: {
     Navigation,
     CustomFooter,
+    CustomCard,
+  },
+  data() {
+    return {
+      places: [
+        {
+          title: "Tikal",
+          img_source:
+            "https://cache-graphicslib.viator.com/graphicslib/page-images/742x525/116000_B_Guatemala_Tikal_shutterstock_000021688951.jpg",
+          description:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate iusto tempora, sed nisi odio non dolorum, voluptatem maxime aut distinctio molestiae ex? Reprehenderit ducimus expedita enim omnis et consequatur nam.",
+          like: 0,
+        },
+        {
+          title: "Tikal",
+          img_source:
+            "https://cache-graphicslib.viator.com/graphicslib/page-images/742x525/116000_B_Guatemala_Tikal_shutterstock_000021688951.jpg",
+          description:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate iusto tempora, sed nisi odio non dolorum, voluptatem maxime aut distinctio molestiae ex? Reprehenderit ducimus expedita enim omnis et consequatur nam.",
+          like: 0,
+        },
+        {
+          title: "Tikal",
+          img_source:
+            "https://cache-graphicslib.viator.com/graphicslib/page-images/742x525/116000_B_Guatemala_Tikal_shutterstock_000021688951.jpg",
+          description:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate iusto tempora, sed nisi odio non dolorum, voluptatem maxime aut distinctio molestiae ex? Reprehenderit ducimus expedita enim omnis et consequatur nam.",
+          like: 0,
+        },
+        {
+          title: "Tikal",
+          img_source:
+            "https://cache-graphicslib.viator.com/graphicslib/page-images/742x525/116000_B_Guatemala_Tikal_shutterstock_000021688951.jpg",
+          description:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate iusto tempora, sed nisi odio non dolorum, voluptatem maxime aut distinctio molestiae ex? Reprehenderit ducimus expedita enim omnis et consequatur nam.",
+          like: 0,
+        },
+        {
+          title: "Tikal",
+          img_source:
+            "https://cache-graphicslib.viator.com/graphicslib/page-images/742x525/116000_B_Guatemala_Tikal_shutterstock_000021688951.jpg",
+          description:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate iusto tempora, sed nisi odio non dolorum, voluptatem maxime aut distinctio molestiae ex? Reprehenderit ducimus expedita enim omnis et consequatur nam.",
+          like: 0,
+        },
+      ],
+    };
   },
 };
 </script>
@@ -28,7 +81,6 @@ export default {
 body {
   margin: 0;
 }
-
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -36,6 +88,6 @@ body {
   color: #2c3e50;
 }
 main {
-  padding-top: 75px;
+  padding-top: 85px;
 }
 </style>
---
+++ src/components/CustomCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="col-md-4 col-sm-12 custom-card">
+    <div class="card">
+      <div class="title">
+        <h3>{{place.title}}</h3>
+      </div>
+      <div class="img">
+        <img :src="place.img_source" :alt="place.title">
+      </div>
+      <div class="content">
+        <p>{{place.description}}</p>
+      </div>
+      <div class="likes">
+        {{place.like}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "customCard",
+  props: ["place"],
+  methods: {
+  },
+};
+</script>
+
+<style scoped>
+img {
+  max-width: 100%;
+}
+.title{
+  text-align: center;
+  border: 1px solid black;
+}
+.custom-card{
+  padding: 10px;
+  background-color: #52b788;
+}
+
+</style>
